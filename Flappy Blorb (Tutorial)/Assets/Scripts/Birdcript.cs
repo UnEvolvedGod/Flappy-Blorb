bd3432324f552de9380e71a3d574357f4a6935da
edgarq1215 <edgarq1215@gmail.com>
2023-03-20 16:20:03 -0400

    Testing flappybird unity in github

---
+++ Flappy Blorb (Tutorial)/Assets/Scripts/Birdcript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdcript : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float height = 6f;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+    public float maxHeight = 14;
+    public float minHeight = -13;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
+        {
+            myRigidBody.velocity = Vector2.up * height;
+        }
+        
+        if(transform.position.y > maxHeight || transform.position.y < minHeight)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+
+}
---
+++ Flappy Blorb (Tutorial)/Assets/Scripts/PipeMiddleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMiddleScript : MonoBehaviour
+{
+    public LogicScript logic;
+    public Birdcript birdcript;
+    
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        birdcript = GameObject.FindGameObjectWithTag("Player").GetComponent<Birdcript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Won't add point if bird goes through pipe after it died
+        if(collision.gameObject.layer == 8 && birdcript.birdIsAlive)
+        {
+            logic.addScore(1);
+        }
+        
+    }
+
+}
